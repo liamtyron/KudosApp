62f77b78e7d49356e58f5a8b127bd8fbede7b874
Ichiko2 <zanelemileyshandu@gmail.com>
2026-05-14 12:12:28 +0200

    submit

--- src/Controller/KudosController.php
+++ src/Controller/KudosController.php
@@ -3,13 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Kudos;
+use App\Entity\User;
 use App\Form\KudosType;
 use App\Repository\KudosRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 #[Route('/kudos')]
 final class KudosController extends AbstractController
@@ -22,25 +26,86 @@ final class KudosController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_kudos_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    // #[Route('/new', name: 'app_kudos_new', methods: ['GET', 'POST'])]
+    // public function new(Request $request, EntityManagerInterface $entityManager, RequestStack $requestStack, UserRepository $userRepository): Response
+    // {
+
+    //     $session = $requestStack->getSession();
+    //     if($request->isMethod('POST')){
+    //     $name = $request->request->get('firstName');
+    //     $user = $userRepository->findOneBy(['firstName' =>$name]);
+    //     $surname = $request->request->get('lastName');
+    //     $user = $userRepository->findOneBy(['laststName' =>$surname]);
+    //     dd($name, $user);
+
+    //    // $user = $userRepository->findOneBy('firstName' =>);
+
+    //     $kudo = new Kudos();
+    //     $form = $this->createForm(KudosType::class, $kudo);
+    //     $form->handleRequest($request);
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $entityManager->persist($kudo);
+    //         $entityManager->flush();
+
+    //         return $this->redirectToRoute('app_kudos_index', [], Response::HTTP_SEE_OTHER);
+    //     }
+    //     }
+    //     return $this->render('kudos/new.html.twig', [
+    //         'kudo' => $kudo,
+    //         'form' => $form,
+           
+    //     ]);
+
+       
+    // }
+     
+
+    //     $session = $requestStack->getSession();
+    //    if($request->isMethod('POST')){
+    //     $email = $request->request->get('email');
+    //     $user = $userRepository->findOneBy(['email' =>$email]);
+    //     //dd($email, $user);
+
+    //     if(!$user){
+    //         $this->addFlash('error', 'No account linked to this email chile');
+    //         return $this->redirectToRoute('app_forgot_pswd');
+    //     }
+
+    //     $session->set('reset_user_id', $user->getId());
+    //     return $this->redirectToRoute('app_update_pswd');
+    //    }
+    //     return $this->render('password/email.html.twig');
+
+        
+
+    #[Route('/new', name: 'app_kudos_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager,): Response
     {
         $kudo = new Kudos();
+        // 1. Create the form BEFORE the if() statement
         $form = $this->createForm(KudosType::class, $kudo);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+           
+            $kudo->setSender($this->getUser());
+
             $entityManager->persist($kudo);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_kudos_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_dashboard', [], Response::HTTP_SEE_OTHER);
         }
 
+        // 2. Return form for both GET and invalid POST
         return $this->render('kudos/new.html.twig', [
             'kudo' => $kudo,
-            'form' => $form,
+            'form' => $form->createView(), // Pass createView()
         ]);
     }
+
+    
+    
 
     #[Route('/{id}', name: 'app_kudos_show', methods: ['GET'])]
     public function show(Kudos $kudo): Response
